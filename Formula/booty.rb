dff069339b20b86cb61b61be49d93ebc5b3edbce
Borislav Kibarov <borislav.kibarov@cleverpine.com>
2024-01-05 10:50:12 +0200

    Update url & sha256

--- Formula/booty.rb
+++ Formula/booty.rb
@@ -1,8 +1,8 @@
 class Booty < Formula
   desc "A Shell application for different framework structure generation"
   homepage "https://github.com/cleverpine/PineBoot"
-  url "https://github.com/cleverpine/Booty/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "d18daf7dda386c32f6d6d6a99fbb6efd636762d3ea80f555133f727603bfb872"
+  url "https://github.com/cleverpine/Booty/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "d81bfbe3bce9ae855a03cfb9890071990f861d9d4c32248d316f4bf905406700"
   license "MIT"
 
   def install
