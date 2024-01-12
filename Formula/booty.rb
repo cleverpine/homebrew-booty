743ed943b2503ab5ec6897e7d76a8b63067330a6
Ivan Nikolov <ivan.nikolov@cleverpine.com>
2024-01-12 14:08:20 +0200

    Update homepage repo

--- Formula/booty.rb
+++ Formula/booty.rb
@@ -1,6 +1,6 @@
 class Booty < Formula
   desc "A Shell application for different framework structure generation"
-  homepage "https://github.com/cleverpine/PineBoot"
+  homepage "https://github.com/cleverpine/Booty"
   url "https://github.com/cleverpine/Booty/archive/refs/tags/v0.0.5.tar.gz"
   sha256 "78acceff4e255c04f7b5ae973b78f65276ebe781581e9fa39d3ce83354a74edb"
   license "MIT"
