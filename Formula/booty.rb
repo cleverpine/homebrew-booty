15a952fca1dbb41ca1ab36095bcc7453dff91c33
ilian-cleverpine <ilian@cleverpine.com>
2024-01-15 14:04:18 +0000

    Update url & sha256 for v0.0.7

--- Formula/booty.rb
+++ Formula/booty.rb
@@ -2,7 +2,7 @@ class Booty < Formula
   desc "A Shell application for different framework structure generation"
   homepage "https://github.com/cleverpine/Booty"
   url "https://github.com/cleverpine/Booty/archive/refs/tags/v0.0.7.tar.gz"
-  sha256 "a6100ec32b76bd45ca0a273febd9048bf48d7e71ddbabc5e3c2be36feb3c0e84"
+  sha256 "cb2ade0d2c1ae9711eb581a91fff8da0fa2529a98afea23ea5d7c17e9f98aac4"
   license "MIT"
 
   def install
